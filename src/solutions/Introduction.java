f48f9dd5ebd01be99583ae9942f51a1c2a0bce4b
Eduardo Richard Silva Porras <eduasilvaporras@frba.utn.edu.ar>
2023-03-01 12:32:04 -0300

    Introduccion a Algoritmos y Java: Punto 1c

--- src/solutions/Introduction.java
+++ src/solutions/Introduction.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Introduction {
+
+    /**
+     * Returns a list of integers within an interval, including endpoints.
+     * @param start integer indicating the start of the interval.
+     * @param end integer indicating the end of the interval.
+     * @return returns a list of integers within an interval defined by start and end.
+     */
     public List<Integer> numbersBetween(int start, int end) { // punto 1-a
         int i = start;
         List<Integer> numbers = new ArrayList<Integer>();
@@ -14,6 +21,12 @@ public class Introduction {
         return numbers;
     }
 
+    /**
+     * Returns a list of even integers within a range, including endpoints.
+     * @param start integer indicating the start of the interval.
+     * @param end integer indicating the end of the interval.
+     * @return returns a list of even integers within an interval defined by start and end.
+     */
     public List<Integer> evenNumbersBetween(int start, int end) { // punto 1.b
         List<Integer> numbers = numbersBetween(start, end);
         List<Integer> pairs = new ArrayList<Integer>();
@@ -27,7 +40,41 @@ public class Introduction {
         return pairs;
     }
 
+    /**
+     * Returns a list of odd integers within a range, including endpoints.
+     * @param start integer indicating the start of the interval.
+     * @param end integer indicating the end of the interval.
+     * @return returns a list of odd integers within an interval defined by start and end.
+     */
+    public List<Integer> oddNumbersBetween(int start, int end) {
+        List<Integer> numbers = numbersBetween(start, end);
+        List<Integer> pairs = evenNumbersBetween(start, end);
+        numbers.removeAll(pairs);
+        return numbers;
+    }
+
+    /**
+     * Returns a list of odd or even integers within a defined range, additionally requires specifying the parity to use.
+     * @param start integer indicating the start of the interval.
+     * @param end integer indicating the end of the interval.
+     * @param parity boolean value indicating the parity to use, being true for even and false for odd.
+     * @return Returns a list of odd or even integers within an interval defined by start and end with a parity given by parity
+     */
+    public List<Integer> filterRangeNumbersBy(int start, int end, String parity) { // punto 1.c
+        List<Integer> filteredList = new ArrayList<Integer>();
+        if(parity == "even") {
+            filteredList = evenNumbersBetween(start, end);
+        } else {
+            filteredList = oddNumbersBetween(start, end);
+        }
+        return filteredList;
+    }
+
     // Funcion Auxiliar  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    /**
+     * Prints a sequence of integers in string format given a list of numbers.
+     * @param numbers list of integers to be formatted
+     */
     public void printInSequence(List<Integer> numbers) { // reutilizable
         int length = numbers.size() - 1;
         int index = 0;
