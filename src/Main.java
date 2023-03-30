b8b741ed64e40fcba4cc13a147bffeaeb75f6b64
Eduardo Richard Silva Porras <eduasilvaporras@frba.utn.edu.ar>
2023-03-30 09:20:30 -0300

    Array y String: Invocaciones de metodos desde Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-import solutions.Introduction;
+import solutions.*;
+import solutions.IntroduccionPOO.Carrito;
+import solutions.IntroduccionPOO.Descuento;
+import solutions.IntroduccionPOO.DescuentoXMontoTotal;
+import solutions.IntroduccionPOO.DescuentoXTarjetaVIP;
+import solutions.IntroduccionPOO.ItemCarrito;
+import solutions.IntroduccionPOO.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        // introduction to algorithms and java
         Introduction intro = new Introduction();
         intro.printInSequence(intro.numbersBetween(1, 10)); // 1-a
         intro.printInSequence(intro.evenNumbersBetween(4, 20)); // 1-b
@@ -13,5 +25,14 @@ public class Main {
         System.out.println(intro.homeBelongsHighIncomeSegment(300000, 2 , 3 , false));
         System.out.println(intro.homeBelongsHighIncomeSegment(300000, 2, 2, true));
         System.out.println(intro.homeBelongsHighIncomeSegment(700000, 5, 7, true));
+
+        // Array and String
+        ArrayAndString collection = new ArrayAndString();
+        System.out.println(collection.numberOccurrencesletterInString("propiedades de java", 'a'));
+        System.out.println(collection.totalOfAddingTheGreaterOf(new int[] {1, 4, 5, 6}, 3));
+        System.out.println(collection.orderNumbers(21, 56, 20, false));
+        System.out.println(collection.encode("hola zeuz como estas", 2));
+        System.out.println(collection.decode("jqncbagwabeqñqbguvcu", 2));
+
     }
 }
